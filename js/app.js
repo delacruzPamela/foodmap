3cb0bc66e67d4273ea259e498c926f9f8932bf8a
delacruzPamela <delacruzlozano.p@gmail.com>
2017-12-30 15:16:35 -0600

    Vista principal logo, splash terminado (css-index-jquery)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+// 1° Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista principal. 
 $(function() {
   setTimeout(function() { 
     window.location.href = 'views/restaurante.html';
-  }, 5000);
+  }, 3000);
 });
+
+
--- js/restaurante.js
+++ js/restaurante.js
@@ -1,7 +1,15 @@
 //
 $(function() {
+  var $filtrarRestaurant = $('#filtrarRestaurant');
+  var $imagesRestaurant = $('div.imagesRestaurant');
   $.each(data, function(district, restaurant) {
-    console.log(i);
-    console.log(j);
+    if ($filtrarRestaurant.val() === district) {
+      $('h2').text(key);
+      var listaRestaurante = data[district];
+      for (var i = 0; i < Object.keys(listaRestaurante).length; i++) {
+        $imagesRestaurant.addClass('style-restaurant').eq(i).text(Object.keys(listaRestaurante)[i]);
+      }
+    };
   });
 });
+
