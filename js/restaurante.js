4c9454f7094036ae57aa57c6813a80b1cedb0df1
delacruzPamela <delacruzlozano.p@gmail.com>
2018-01-02 13:07:16 -0600

    Ultimos cambios

--- js/restaurante.js
+++ js/restaurante.js
@@ -1,5 +1,5 @@
 // Slider de header
-$(function() {
+$(document).ready(function() {
   $('.myCarousel').carousel({
     interval: 1000
   });
@@ -11,7 +11,7 @@ $(function() {
     $.each(data[district], function(nombreLocal) {
       var imagen = data[district][nombreLocal].logo;
       // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-3'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
-      var restaurantContainer = "<div class='restaurant-info collection'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
+      var restaurantContainer = "<div class='restaurant-info collection'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target ='#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
       // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-6'><a data-distrito='' + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target ='#infoModal'><div class='data'><p><strong>Restaurant El Charapito</strong></p><p>Jirón Ayacucho 198</p><p>Abierto hasta las 19:00</p></div><div class='photo-restaurant'></div></a></div>";
       $('#list-restaurant .row .restaurant-container').append(restaurantContainer);
     });
@@ -38,4 +38,11 @@ $(function() {
       }
     });
   });
+
+  // Efecto Mouseover
+  $('.restaurant-info').mouseover(function() {
+    $('.restaurant-info').css('background-color', 'transparent');
+  });
+
+  // Modal información
 });
