998d719a7d98b602c616176b24e21af79216728f
delacruzPamela <delacruzlozano.p@gmail.com>
2018-01-02 13:17:07 -0600

    Ultimas actualizaciones

--- js/restaurante.js
+++ js/restaurante.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.myCarousel').carousel({
     interval: 1000
   });
-
+  
   /* ACCEDIENDO A LA DATA 
   ---------------------------------------------------------------------------------- */
   // Creando contenedor para mostrar todos los restaurantes ezistentes de la data
@@ -12,7 +12,6 @@ $(document).ready(function() {
       var imagen = data[district][nombreLocal].logo;
       // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-3'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
       var restaurantContainer = "<div class='restaurant-info collection'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target ='#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
-      // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-6'><a data-distrito='' + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target ='#infoModal'><div class='data'><p><strong>Restaurant El Charapito</strong></p><p>Jirón Ayacucho 198</p><p>Abierto hasta las 19:00</p></div><div class='photo-restaurant'></div></a></div>";
       $('#list-restaurant .row .restaurant-container').append(restaurantContainer);
     });
   };
