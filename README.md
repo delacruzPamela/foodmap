dda18d14c8820d6c0f86a768f5eef7e7a35779f1
delacruzPamela <delacruzlozano.p@gmail.com>
2018-01-02 06:06:13 -0600

    Añadiendo readme de foofmap

---
+++ README.md
@@ -0,0 +1,48 @@
+# RETO 1: Foodmap
+
+* **Curso:** _Responsive web Design_
+* **Unidad 3:** _Agiliza tu desarrollo_
+* **Tema** _Reto N° 1 -USO DE JQUERY 
+
+***
+## OBJETIVO
+* Recrear una web-app que a través de un input pueda filtrar los restaurantes que se encuentren cerca de ti. 
+
+## Flujo de trabajo
+* Creamos la página utilizando Bootstrap para hacer responsive junto al diseño css. 
+
+* **Vista splash** con duración de 2 a 5 segundos que redirecciona a tu vista principal.
+
+* En la vista principal se muestran todos los **restaurantes "cerca de ti"** junto con el input para filtrar tu elección.
+
+* Agrega un evento **mouseover** a las imágenes (Cambio de tamaño, muestra de texto, cambio de imagen, información, etc). 
+
+* Al seleccionar alguno de los restaurantes, deberá mostrarse la información de este a través de **un modal**. 
+
+* Una vez cerrado el modal debe **volver a la vista principal**. 
+
+ 
+## Herramientas utilizadas
+
+* HTML 5
+* CSS 3
+* Bootstrap
+* JQuery
+
+## PRODUCTO FINAL
+
+#### VERSIÓN MOVIL
+
+#### MODALES
+
+* Inicia Sesión
+
+![recursos](assets/images/splash.PNG)
+
+* Filtrar restaurante
+
+![recursos](assets/images/filtrar.PNG)
+
+* Efecto mouseover
+
+* Información mediante un modal
--- js/restaurante.js
+++ js/restaurante.js
@@ -11,7 +11,7 @@ $(function() {
     $.each(data[district], function(nombreLocal) {
       var imagen = data[district][nombreLocal].logo;
       // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-3'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
-      var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-3'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
+      var restaurantContainer = "<div class='restaurant-info collection'><a data-distrito='" + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target = '#infoModal'> <span class='caption'><span>" + nombreLocal + "</span><img class='center-block' src=' "+ imagen + "' alt='Info'></span></a></div>";
       // var restaurantContainer = "<div class='restaurant-info collection col-xs-12 col-md-6'><a data-distrito='' + district + "' data-local= '" + nombreLocal + "' href ='#' data-toggle ='modal' data-target ='#infoModal'><div class='data'><p><strong>Restaurant El Charapito</strong></p><p>Jirón Ayacucho 198</p><p>Abierto hasta las 19:00</p></div><div class='photo-restaurant'></div></a></div>";
       $('#list-restaurant .row .restaurant-container').append(restaurantContainer);
     });
